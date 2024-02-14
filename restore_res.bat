65297673470278fc3c7280ed9aa40d086d105ea1
danilo-dellorco <danilodellorcolp@gmail.com>
2024-02-14 13:40:34 +0100

    Add Automatic Res and Ratio Detection

--- restore_res.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-python restore_res.py
--- run_pal.py
+++ run_pal.py
@@ -1,7 +1,79 @@
+import win32api
+import win32con
+import configparser
+import psutil
 import subprocess
 import time
-import psutil
-import configparser
+
+RESOLUTION_MAP = {
+    "4K": (3840, 2160),
+    "2K": (2560, 1440),
+    "1080p": (1920, 1080),
+    "720p": (1280, 720)
+}
+
+def get_physical_primary_monitor_dimensions():
+    dm = win32api.EnumDisplaySettings(None, win32con.ENUM_CURRENT_SETTINGS)
+    screen_width = dm.PelsWidth
+    screen_height = dm.PelsHeight
+    return screen_width, screen_height
+
+def get_screen_ratio():
+    screen_width, screen_height = get_physical_primary_monitor_dimensions()
+    screen_ratio = screen_width / screen_height
+    return screen_ratio
+
+def map_screen_ratio_to_format(screen_ratio):
+    ratio_map = {
+        16 / 10: "16:10",
+        16 / 9: "16:9",
+        4 / 3: "4:3",
+        5 / 4: "5:4",
+        21 / 9: "21:9"
+        # Aggiungi altri rapporti e formati se necessario
+    }
+    return ratio_map.get(screen_ratio, "Formato sconosciuto")
+
+def calculate_resolutions():
+    resolutions = {}
+    
+    for resolution_name, resolution in RESOLUTION_MAP.items():
+        initial_width, initial_height = resolution
+        
+        # Calcola risoluzione per 16:9
+        resolutions_169 = (initial_width, initial_height)
+        
+        # Calcola risoluzione per 16:10
+        resolutions_1610 = (initial_width, initial_width / (16 / 10))
+        
+        # Calcola risoluzione per 4:3
+        resolutions_43 = (initial_width, initial_width / (4 / 3))
+        
+        # Calcola risoluzione per 5:4
+        resolutions_54 = (initial_width, initial_width / (5 / 4))
+        
+        # Calcola risoluzione per 21:9
+        resolutions_219 = (initial_width, initial_width / (21 / 9))
+        
+        # Aggiungi le risoluzioni alla mappa
+        resolutions[f"{resolution_name}_16:9"] = resolutions_169
+        resolutions[f"{resolution_name}_16:10"] = resolutions_1610
+        resolutions[f"{resolution_name}_4:3"] = resolutions_43
+        resolutions[f"{resolution_name}_5:4"] = resolutions_54
+        resolutions[f"{resolution_name}_21:9"] = resolutions_219
+    
+    return resolutions
+
+# Read parameters from config.ini
+def read_config(filename):
+    config = configparser.ConfigParser()
+    config.read(filename)
+
+    qres = config.get('Paths', 'QRes')
+    palw = config.get('Paths', 'Palw')
+    res = config.get("Resolution", "GameRes")
+    
+    return qres, palw, res
 
 def checkIfProcessRunning(processName):
     '''
@@ -17,25 +89,21 @@ def checkIfProcessRunning(processName):
             pass
     return False
 
-# Read parameters from config.ini
-def read_config(filename):
-    config = configparser.ConfigParser()
-    config.read(filename)
-
-    qres = config.get('Paths', 'QRes')
-    palw = config.get('Paths', 'Palw')
-    resolution_x = config.get('Resolution', 'ResXGame')
-    resolution_y = config.get('Resolution', 'ResYGame')
-
-    return qres, palw, resolution_x, resolution_y
-
 if __name__ == "__main__":
-    # Read parameters from config.ini
-    qres, palw, resolution_x, resolution_y = read_config('config.ini')
-
+    qres, palw, res = read_config('config.ini')
+    resolutions_map = calculate_resolutions()
+    
+    screen_ratio = get_screen_ratio()
+    screen_format = map_screen_ratio_to_format(screen_ratio)
+    
+    target_res = resolutions_map[f'{res}_{screen_format}']
+    
+    origin_w, origin_h = get_physical_primary_monitor_dimensions()
+    target_w, target_h = target_res[0], target_res[1]
+    
     # Change resolution
-    subprocess.run([qres, f"/x:{resolution_x}", f"/y:{resolution_y}"])
-
+    subprocess.run([qres, f"/x:{target_w}", f"/y:{target_h}"])
+    
     # Start Palworld
     subprocess.run(["start", "/WAIT", "", palw], shell=True)
 
@@ -50,4 +118,4 @@ if __name__ == "__main__":
         time.sleep(2)
 
     # Restore original resolution
-    subprocess.run([qres, "/x:3840", "/y:2400"])
+    subprocess.run([qres, f"/x:{origin_w}", f"/y:{origin_h}"])
